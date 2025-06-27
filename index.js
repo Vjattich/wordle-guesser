749941284d03f85fcd0b0d4919c0cef83215fb00
vjattich <sektaaceel@gmail.com>
2025-06-28 01:33:18 +0500

    fixes

--- index.js
+++ index.js
@@ -178,7 +178,7 @@ const walkie = function (self, event) {
     e.focus();
 };
 
-const keyDownEvent = function (e) {
+const keyPressEvent = function (e) {
 
     let self = this,
         event = toEvent(e);
@@ -473,7 +473,7 @@ const addEvents = function () {
     inputs.forEach(input => {
         input.addEventListener("click", inputEvent)
         input.addEventListener("input", inputEvent)
-        input.addEventListener("keydown", keyDownEvent)
+        input.addEventListener("keypress", keyPressEvent)
         input.addEventListener("keyup", keyUpEvent)
         input.addEventListener("focus", function () {
             setTimeout(() => {
